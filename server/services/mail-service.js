b2c2c45b836c59f870015111d63a41b87085fda2
schiZis <stahanovgod@gmail.com>
2022-07-17 17:57:03 +0500

    added ability to send email with activation link

--- server/services/mail-service.js
+++ server/services/mail-service.js
@@ -1,7 +1,34 @@
-
+const nodemailer = require('nodemailer');
 
 class MailService {
-    async sendActivationMail(to, link) {};
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            }
+        })
+    }
+
+    async sendActivationMail(to, link) {
+        this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to,
+            subject: 'Testing activation via email from ' + process.env.API_URL,
+            text: '',
+            html:
+                `
+                    <div>
+                        <h1>Для активации перейдите по ссылке</h1>
+                        <a href="${link}">&{link}</a>
+                    </div>
+                `
+        })
+    };
 }
 
 module.exports = new MailService();
