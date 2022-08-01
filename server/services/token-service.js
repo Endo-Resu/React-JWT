ad654204dfc2aea1208504971c01c9b6f48e3b2e
schiZis <stahanovgod@gmail.com>
2022-08-01 10:41:28 +0500

    finally get some time to ended server-side: refresh tokens and validation for 'em, all users info

--- server/services/token-service.js
+++ server/services/token-service.js
@@ -11,6 +11,23 @@ class TokenService {
         }
     }
 
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_KEY);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_KEY)
+        } catch (e) {
+            return null
+        }
+    }
+
     async saveToken(userId, refreshToken) {
         const tokenData = await tokenModel.findOne({user: userId})
         if (tokenData) {
@@ -23,6 +40,11 @@ class TokenService {
 
     async removeToken(refreshToken) {
         const tokenData = await tokenModel.deleteOne({refreshToken});
+        return tokenData;
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await tokenModel.findOne({refreshToken});
         return tokenData;
     }
 }
